e8d04680071a56968d2346d660999b23fc6b17b3
Jaerock Kwon <jkwon@kettering.edu>
2019-02-05 16:39:41 -0500

    Fix 0-dim tensor warnings

--- models/recycle_gan_model.py
+++ models/recycle_gan_model.py
@@ -205,8 +205,8 @@ class RecycleGANModel(BaseModel):
         pred_B = self.fake_B_pool.query(self.pred_B2)
         loss_D_A3 = self.backward_D_basic(self.netD_A, self.real_B2, pred_B)
 
-        self.loss_D_A = loss_D_A0.data[0] + loss_D_A1.data[0] + \
-            loss_D_A2.data[0] + loss_D_A3.data[0]
+        self.loss_D_A = loss_D_A0.item() + loss_D_A1.item() + \
+            loss_D_A2.item() + loss_D_A3.item()
 
     def backward_D_B(self):
         fake_A0 = self.fake_A_pool.query(self.fake_A0)
@@ -221,8 +221,8 @@ class RecycleGANModel(BaseModel):
         pred_A = self.fake_A_pool.query(self.pred_A2)
         loss_D_B3 = self.backward_D_basic(self.netD_B, self.real_A2, pred_A)
 
-        self.loss_D_B = loss_D_B0.data[0] + loss_D_B1.data[0] + \
-            loss_D_B2.data[0] + loss_D_B3.data[0]
+        self.loss_D_B = loss_D_B0.item() + loss_D_B1.item() + \
+            loss_D_B2.item() + loss_D_B3.item()
 
     def backward_G(self):
         lambda_idt = self.opt.identity
@@ -319,14 +319,14 @@ class RecycleGANModel(BaseModel):
         self.rec_A = rec_A.data
         self.rec_B = rec_B.data
 
-        self.loss_G_A = loss_G_A0.data[0] + \
-            loss_G_A1.data[0] + loss_G_A2.data[0]
-        self.loss_G_B = loss_G_B0.data[0] + \
-            loss_G_B1.data[0] + loss_G_B2.data[0]
-        self.loss_cycle_A = loss_cycle_A.data[0]
-        self.loss_cycle_B = loss_cycle_B.data[0]
-        self.loss_pred_A = loss_pred_A.data[0]
-        self.loss_pred_B = loss_pred_B.data[0]
+        self.loss_G_A = loss_G_A0.item() + \
+            loss_G_A1.item() + loss_G_A2.item()
+        self.loss_G_B = loss_G_B0.item() + \
+            loss_G_B1.item() + loss_G_B2.item()
+        self.loss_cycle_A = loss_cycle_A.item()
+        self.loss_cycle_B = loss_cycle_B.item()
+        self.loss_pred_A = loss_pred_A.item()
+        self.loss_pred_B = loss_pred_B.item()
 
     def optimize_parameters(self):
         # forward
